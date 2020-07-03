71184999544c4595f1efa0e8ed2dba84a614d6b2
Junsuk Oh <a811219@gmail.com>
2020-07-03 19:12:26 +0900

    카운터 앱의 문제

--- app/src/main/java/com/company/counter/MainActivity.kt
+++ app/src/main/java/com/company/counter/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.company.counter
 
+import android.app.Activity
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
 
     var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: ")
 
         add_button.setOnClickListener {
             count++
@@ -21,5 +30,36 @@ class MainActivity : AppCompatActivity() {
             count--
             counter_text.text = "$count"
         }
+
+        registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityPaused(activity: Activity) {
+                Log.d(TAG, "onActivityPaused: ")
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+                Log.d(TAG, "onActivityStarted: ")
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+                Log.d(TAG, "onActivityDestroyed: ")
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+                Log.d(TAG, "onActivitySaveInstanceState: ")
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+                Log.d(TAG, "onActivityStopped: ")
+            }
+
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                Log.d(TAG, "onActivityCreated: ")
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+                Log.d(TAG, "onActivityResumed: ")
+            }
+
+        })
     }
 }
